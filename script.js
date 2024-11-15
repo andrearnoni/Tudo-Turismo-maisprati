0b16a530d7e8d0d0b4c6886b529736fb77986282
andrearnoni <andre_arnoni@yahoo.com.br>
2024-11-14 21:58:19 -0300

    adicionada a api de random users

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const API_URL = "https://randomuser.me/api/";
+
+  async function fetchTestimonial() {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      const testimonial = data.results[0];
+      return testimonial;
+    } catch (error) {
+      console.error("Erro ao buscar relato:", error);
+      return null;
+    }
+  }
+
+  async function fetchAndDisplayTestimonials() {
+    const testimonials = await Promise.all([
+      fetchTestimonial(),
+      fetchTestimonial(),
+      fetchTestimonial(),
+    ]);
+
+    testimonials.forEach((testimonial, index) => {
+      if (testimonial) {
+        const card = document.querySelectorAll(".card-testemunho")[index];
+
+        const imgElement = card.querySelector("img");
+        imgElement.src = testimonial.picture.medium;
+        imgElement.alt = `${testimonial.name.first} ${testimonial.name.last}`;
+
+        const nameElement = card.querySelector("h5");
+        nameElement.textContent = `${testimonial.name.first} ${testimonial.name.last}`;
+
+        const locationElement = card.querySelector("h6");
+        locationElement.textContent = `${testimonial.location.city} - ${testimonial.location.state}`;
+      }
+    });
+  }
+
+  fetchAndDisplayTestimonials();
+});
+
 function mostrarMenu() {
   $("nav#nav-esquerda ul.menu-principal").css("display", "flex");
   $("nav#nav-esquerda ul.menu-principal").addClass(
